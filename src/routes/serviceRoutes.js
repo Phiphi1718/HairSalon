b20c33eb978dc08dcd43336d58eb5a205f5aef48
Phiphi1718 <86730408+Phiphi1718@users.noreply.github.com>
2025-03-02 15:58:25 +0700

    Update serviceRoutes.js

--- src/routes/serviceRoutes.js
+++ src/routes/serviceRoutes.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const serviceController = require('../controllers/serviceController');
-const { authenticate, isAdmin } = require('../middlewares/authMiddleware');
+const { authMiddleware, isAdmin } = require('../middlewares/authMiddleware');
 
 // Người dùng bình thường có thể lấy thông tin dịch vụ
-router.get('/', authenticate, serviceController.getAllServices);
-router.get('/:name', authenticate, serviceController.getServiceByName);
-router.post('/', authenticate, serviceController.createService);
+router.get('/', authMiddleware, serviceController.getAllServices);
+router.get('/:name', authMiddleware, serviceController.getServiceByName);
+router.post('/', authMiddleware, serviceController.createService);
 
 // Chỉ admin mới có quyền thêm, cập nhật và xóa dịch vụ
-router.put('/:name', authenticate, isAdmin, serviceController.updateServiceByName);
-router.delete('/:name', authenticate, isAdmin, serviceController.deleteServiceByName);
+router.put('/:name', authMiddleware, isAdmin, serviceController.updateServiceByName);
+router.delete('/:name', authMiddleware, isAdmin, serviceController.deleteServiceByName);
 
 module.exports = router;
