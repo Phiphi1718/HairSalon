ececa98802d044c4273f40f53e9f9b488f91cc2b
Phiphi1718 <86730408+Phiphi1718@users.noreply.github.com>
2025-03-02 15:57:49 +0700

    Update productRoutes.js

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
-const { authenticate, isAdmin } = require('../middlewares/authMiddleware');
+const { authMiddleware, isAdmin } = require('../middlewares/authMiddleware');
 
 // 🔹 Chỉ khách hàng mới có thể lấy sản phẩm
-router.get('/', authenticate, productController.getAllProducts);
-router.get('/:name', authenticate, productController.getProductByName);
+router.get('/', authMiddleware, productController.getAllProducts);
+router.get('/:name', authMiddleware, productController.getProductByName);
 
 // 🔹 Chỉ admin mới có thể thao tác CRUD
-router.post('/', authenticate, isAdmin, productController.createProduct);
-router.put('/:name', authenticate, isAdmin, productController.updateProductByName);
-router.delete('/:name', authenticate, isAdmin, productController.deleteProductByName);
+router.post('/', authMiddleware, isAdmin, productController.createProduct);
+router.put('/:name', authMiddleware, isAdmin, productController.updateProductByName);
+router.delete('/:name', authMiddleware, isAdmin, productController.deleteProductByName);
 
 module.exports = router;
