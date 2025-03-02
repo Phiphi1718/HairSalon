e17f8c58ce818604e73297e5bb4f385f9cae6e9b
Phiphi1718 <86730408+Phiphi1718@users.noreply.github.com>
2025-03-02 15:58:05 +0700

    Update reviewRoutes.js

--- src/routes/reviewRoutes.js
+++ src/routes/reviewRoutes.js
@@ -4,7 +4,7 @@ const reviewController = require('../controllers/reviewController');
 const { authMiddleware, isAdmin, authenticate } = require("../middlewares/authMiddleware");
 
 
-router.post('/',authMiddleware, authenticate, reviewController.createReview);
+router.post('/',authMiddleware, reviewController.createReview);
 router.get('/', reviewController.getAllReviews);
 router.get('/:username',authMiddleware, isAdmin, reviewController.getReviewsByUsername);
 router.put('/:id',authMiddleware, isAdmin, reviewController.updateReview);
