e555f801ad7958d289da8aa165b53f9048f17a89
Phiphi1718 <86730408+Phiphi1718@users.noreply.github.com>
2025-03-02 16:01:46 +0700

    Update appointmentRoutes.js

--- src/routes/appointmentRoutes.js
+++ src/routes/appointmentRoutes.js
@@ -10,7 +10,7 @@ const { authMiddleware, isAdmin } = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
-router.get('/all', authMiddleware, isAdmin, getAllAppointments);  // Chỉ admin được xem
+router.get('/all', authMiddleware, getAllAppointments);  // Chỉ admin được xem
 router.post('/create', authMiddleware, createAppointment);       // Ai cũng có thể đặt lịch hẹn
 router.put('/update', authMiddleware, isAdmin, updateAppointment);  // Chỉ admin có thể cập nhật
 router.delete('/delete', authMiddleware, isAdmin, deleteAppointment);  // Chỉ admin có thể xóa
