67a95a9b899f4005d20062b390a108dcb5d08b1d
Phiphi1718 <86730408+Phiphi1718@users.noreply.github.com>
2025-03-04 20:41:26 +0700

    Update authController.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -61,7 +61,7 @@ const authController = {
         res.cookie("refreshToken", refreshToken, {
             httpOnly: true,
             secure: false,
-            sameSite: "Lax",
+            sameSite: "None",
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
