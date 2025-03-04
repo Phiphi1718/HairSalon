31822dc9845515f5681ecec8da064bfa395ed909
Phiphi1718 <86730408+Phiphi1718@users.noreply.github.com>
2025-03-04 16:37:31 +0700

    Update productModel.js

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -14,9 +14,12 @@ const Product = {
   },
 
   // Thêm sản phẩm
-  async create({ name, description, price, stock, image_url }) {
+  async create: async (product) => {
+    const { name, description, price, stock, image_url } = product;
     const result = await pool.query(
-      'INSERT INTO products (name, description, price, stock, image_url) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+      `INSERT INTO products (name, description, price, stock, image_url)
+       VALUES ($1, $2, $3, $4, $5)
+       RETURNING *`,
       [name, description, price, stock, image_url]
     );
     return result.rows[0];
