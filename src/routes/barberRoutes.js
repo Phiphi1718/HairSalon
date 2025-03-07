fab7df345f02581f87450a28638d1c3999b8b186
Phiphi1718 <86730408+Phiphi1718@users.noreply.github.com>
2025-03-07 20:15:26 +0700

    Update barberRoutes.js

--- src/routes/barberRoutes.js
+++ src/routes/barberRoutes.js
@@ -2,14 +2,12 @@ const express = require("express");
 const router = express.Router();
 const barberController = require("../controllers/barberController");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
+const { upload } = require("../cloudinary");
 
-// Route không cần quyền admin
 router.get("/barbers", barberController.getAllBarbers);
-
-// Route cần xác thực và quyền admin (authMiddleware trước, isAdmin sau)
 router.get("/barbers/:id", authMiddleware, isAdmin, barberController.getBarberById);
-router.post("/barbers", authMiddleware, isAdmin, barberController.createBarber);
-router.put("/barbers/:id", authMiddleware, isAdmin, barberController.updateBarber);
+router.post("/barbers", authMiddleware, isAdmin, upload.single("image"), barberController.createBarber);
+router.put("/barbers/:id", authMiddleware, isAdmin, upload.single("image"), barberController.updateBarber);
 router.delete("/barbers/:id", authMiddleware, isAdmin, barberController.deleteBarber);
 
 module.exports = router;
