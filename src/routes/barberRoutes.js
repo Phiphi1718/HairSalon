1691ec427c233acc55b7f2f42968e4c12b96c20b
Phiphi1718 <86730408+Phiphi1718@users.noreply.github.com>
2025-03-07 19:46:56 +0700

    Update barberRoutes.js

--- src/routes/barberRoutes.js
+++ src/routes/barberRoutes.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const barberController = require('../controllers/barberController');
-const { isAdmin } = require('../middlewares/authMiddleware');
+const barberController = require("../controllers/barberController");
+const { isAdmin } = require("../middlewares/authMiddleware");
 
-router.get('/barbers',barberController.getAllBarbers);
-router.get('/barbers/:id',isAdmin, barberController.getBarberById);
-router.post('/barbers', isAdmin,barberController.createBarber);
-router.put('/barbers/:id',isAdmin, barberController.updateBarber);
-router.delete('/barbers/:id',isAdmin, barberController.deleteBarber);
+router.get("/barbers", barberController.getAllBarbers);
+router.get("/barbers/:id", isAdmin, barberController.getBarberById);
+router.post("/barbers", isAdmin, barberController.createBarber);
+router.put("/barbers/:id", isAdmin, barberController.updateBarber);
+router.delete("/barbers/:id", isAdmin, barberController.deleteBarber);
 
 module.exports = router;
