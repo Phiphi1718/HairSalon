7ba2f647f09df43240c1208c1fb58107d04e2aff
Phiphi1718 <86730408+Phiphi1718@users.noreply.github.com>
2025-03-09 15:30:32 +0700

    Update serviceRoutes.js

--- src/routes/serviceRoutes.js
+++ src/routes/serviceRoutes.js
@@ -6,7 +6,7 @@ const { authMiddleware, isAdmin } = require('../middlewares/authMiddleware');
 // Người dùng bình thường có thể lấy thông tin dịch vụ
 router.get('/getall', authMiddleware, serviceController.getAllServices);
 router.get('/:name', authMiddleware, serviceController.getServiceByName);
-router.post('/create', authMiddleware, serviceController.createService);
+router.post('/create', authMiddleware, isAdmin, serviceController.createService);
 
 // Chỉ admin mới có quyền thêm, cập nhật và xóa dịch vụ
 router.put('/:name', authMiddleware, isAdmin, serviceController.updateServiceByName);
