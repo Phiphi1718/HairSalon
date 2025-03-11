44d6c15b02fd2e35931aceffc9e4aec0da84b6eb
Phiphi1718 <86730408+Phiphi1718@users.noreply.github.com>
2025-03-11 19:49:19 +0700

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -5,7 +5,7 @@ let io;
 function initSocket(server) {
   io = socketIo(server, {
     cors: {
-      origin: process.env.FRONTEND_URL || "http://localhost:3000",
+      origin: process.env.FRONTEND_URL || "https://hair-salon-forntend.vercel.app",
       methods: ["GET", "POST"],
       credentials: true,
     },
