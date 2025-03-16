a2486706873490d0ff873071d3cfa0599a26a699
phiphi <86730408+tangocphiphilhu-122000550@users.noreply.github.com>
2025-03-16 22:28:01 +0700

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -9,27 +9,15 @@ function initSocket(server) {
     "https://hair-salon-frontend.vercel.app",
   ];
 
-  io = new Server(server, {
+ io = socketIo(server, {
     cors: {
-      origin: function (origin, callback) {
-        console.log("Socket CORS Request Origin:", origin);
-        if (!origin) {
-          console.log("No origin provided, allowing request");
-          return callback(null, true);
-        }
-        if (allowedOrigins.includes(origin)) {
-          console.log(`Origin ${origin} allowed for Socket.io`);
-          return callback(null, origin);
-        } else {
-          console.log(`Socket CORS Error: Origin ${origin} not allowed`);
-          return callback(new Error(`Socket CORS Error: Origin ${origin} not allowed`));
-        }
-      },
-      methods: ["GET", "POST", "OPTIONS"],
-      allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"],
+      origin: [
+        process.env.FRONTEND_URL || "https://hair-salon-forntend.vercel.app",
+        "http://localhost:3000", // Cho phép cả localhost
+      ],
+      methods: ["GET", "POST"],
       credentials: true,
     },
-    transports: ["polling", "websocket"],
   });
 
   io.on("connection", (socket) => {
